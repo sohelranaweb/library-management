845966adaf9ad106449f1e63cf9b86c8ff8e0e7c
sohelranaweb <sohel.rana.web2@gmail.com>
2025-07-09 13:56:04 +0600

    cors origin set

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,10 @@ import mongoose from "mongoose";
 import config from "./config";
 import routes from "./modules/routes";
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173', // your frontend origin
+  credentials: true,
+}));
 app.use(express.json());
 app.use(routes);
 
